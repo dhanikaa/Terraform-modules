2cf38564bdc080d42c3b222a6548619863d6a2b5
Dhanika Kumarasiri <dhanikaanupama2000@gmail.com>
2024-10-15 15:48:50 +0530

    Used modules

--- main.tf
+++ main.tf
@@ -2,10 +2,8 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "example" {
-  ami = var.ami_value
-  instance_type = var.instance_type_value
-  tags = {
-    Name = "modules-example"
-  }
+module "ec2_instance" {
+    source = "./modules/ec2-instance"
+    ami_value = ami-02db68a01488594c5
+    instance_type_value = t3.micro
 }
